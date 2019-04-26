496644ba0dc00fa1609251b5a6d545537146b406
anujverma1710 <anujverma1710@gmail.com>
2019-04-26 14:14:02 -0400

    Saving changes for flask server

---
+++ flaskDirectory/static/js/script.js
@@ -0,0 +1,18 @@
+function american_society() {
+    this.init()
+}
+
+american_society.prototype = {
+    init() {
+        console.log("Helllooooooo")
+        // d3.select("#plotter")
+        //     .on("change", this.getDataToShow);
+        // d3.select("#sampler")
+        //     .on("change", this.getDataToShow);
+        // d3.select("#screeCheck")
+        //     .on("change", this.getDataToShow);
+        // d3.select("#highestCheck")
+        //     .on("change", this.getDataToShow);
+    }
+}
+
