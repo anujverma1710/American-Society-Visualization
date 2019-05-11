ab71cdca7dbc234928f3e1c9b4fcde75796cf30c
anujverma1710 <anujverma1710@gmail.com>
2019-05-11 16:41:10 -0400

    proper map implementation with legends

--- flaskDirectory/static/js/script.js
+++ flaskDirectory/static/js/script.js
@@ -8,13 +8,18 @@ american_society.prototype = {
 
         d3.select("#plotter")
             .on("change", this.getDataToShow);
+
+        d3.select("#year")
+            .on("change", this.getDataToShow);
+
         this.getDataToShow();
 
     },
-    getDataToShow() {
-        console.log($('#plotter').val())
+	getDataToShow() {
         var attr = $('#plotter').val()
-        get_map('/display_plots', attr);
+		var year = $('#year').val()
+
+        get_map('/display_plots', attr, year);
     }
 }
 
@@ -37,10 +42,10 @@ function tooltipHtml(n, d, attr){	/* function to create html content string in t
             "<tr><td>Population </td><td>&nbsp;</td><td>"+(d.Population)+"</td></tr>"+
 			"</table>";
 		case "Race": return "<h4>"+n+"</h4><table>"+
-            "<tr><td style='width: 100px'>Af-Am Ratio </td><td>&nbsp;</td><td>"+(d.AA_Ratio)+"</td></tr>"+
-			"<tr><td style='width: 100px'>Am-Ind Ratio </td><td>&nbsp;</td><td>"+(d.AI_Ratio)+"</td></tr>"+
-			"<tr><td style='width: 100px'>As-Pc Ratio </td><td>&nbsp;</td><td>"+(d.APAC_Ratio)+"</td></tr>"+
-			"<tr><td style='width: 100px'>White Ratio </td><td>&nbsp;</td><td>"+(d.W_Ratio)+"</td></tr>"+
+            "<tr><td style='width: 100px'>Af-Am Ratio </td><td>&nbsp;</td><td>"+d.AA_Ratio+"</td></tr>"+
+			"<tr><td style='width: 100px'>Am-Ind Ratio </td><td>&nbsp;</td><td>"+d.AI_Ratio+"</td></tr>"+
+			"<tr><td style='width: 100px'>As-Pc Ratio </td><td>&nbsp;</td><td>"+d.APAC_Ratio+"</td></tr>"+
+			"<tr><td style='width: 100px'>White Ratio </td><td>&nbsp;</td><td>"+d.W_Ratio+"</td></tr>"+
             "<tr><td style='width: 100px'>Population </td><td>&nbsp;</td><td>"+(d.Population)+"</td></tr>"+
 			"</table>";
 
@@ -49,6 +54,60 @@ function tooltipHtml(n, d, attr){	/* function to create html content string in t
 
 function draw_usa_map(data, attr){
     var sampleData ={};	/* Sample random data. */
+	var Fips = {
+			"Alabama": 1,
+			"Alaska" :2,
+			"Arizona":4,
+			"Arkansas":5,
+			"California":6,
+			"Colorado":8,
+			"Connecticut":9,
+			"Delaware":10,
+			"District Of Columbia":11,
+			"Florida":12,
+			"Georgia":13,
+			"Hawaii":15,
+			"Idaho":16,
+			"Illinois":17,
+			"Indiana":18,
+			"Iowa":19,
+			"Kansas":20,
+			"Kentucky":21,
+			"Louisiana":22,
+			"Maine":23,
+			"Maryland":24,
+			"Massachusetts":25,
+			"Michigan":26,
+			"Minnesota":27,
+			"Mississippi":28,
+			"Missouri":29,
+			"Montana":30,
+			"Nebraska":31,
+			"Nevada":32,
+			"New Hampshire":33,
+			"New Jersey":34,
+			"New Mexico":35,
+			"New York":36,
+			"North Carolina":37,
+			"North Dakota":38,
+			"Ohio":39,
+			"Oklahoma":40,
+			"Oregon":41,
+			"Pennsylvania":42,
+			"Rhode Island":44,
+			"South Carolina":45,
+			"South Dakota":46,
+			"Tennessee":47,
+			"Texas":48,
+			"Utah":49,
+			"Vermont":50,
+			"Virginia":51,
+			"Washington":53,
+			"West Virginia":54,
+			"Wisconsin":55,
+			"Wyoming":56
+		};
+	var testData = []
 	var totalPopulationOfUs = {}
 	var totalMalePopulation = {}
 	var totalFemalePopulation = {}
@@ -58,6 +117,9 @@ function draw_usa_map(data, attr){
 		totalFemalePopulation[i]=0
 	}
 	d3.select('#statesvg').remove();
+
+	d3.select('.legends').remove();
+
 
 	["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
 	"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
@@ -73,48 +135,53 @@ function draw_usa_map(data, attr){
 					totalFemalePopulation[i] = totalFemalePopulation[i] + data[d]["Female_Ratio_" + i] * data[d]["Total_Population_" + i]
 				}
 			}
-			sampleData[d] = getDataBasedonAttribute(attr,data[d],sampleData)
-
+			sampleData[d] = getDataBasedonAttribute(attr,data[d],sampleData, Fips)
+			testData.push(sampleData[d])
 		});
 
-	console.log("Final : ", totalPopulationOfUs)
+	console.log("sampleData : ", sampleData)
 
-	$('#yearSpan').text("1980")
-	$('#popSpan').text(fnum(totalPopulationOfUs[1980]))
+	$('#yearSpan').text($('#year').val())
+	$('#popSpan').text(fnum(totalPopulationOfUs[2010]))
 	if(attr =="Sex"){
-		var temp = (totalMalePopulation["1980"]/totalFemalePopulation["1980"])*100
+		var temp = (totalMalePopulation["2010"]/totalFemalePopulation["2010"])*100
 		$('#ratioSpan').text(temp.toFixed(2))
 	}
 	$('#ratioSpanLabel').text(attr +" Ratio")
 
-
+	loadChloropethMap(testData, tooltipHtml,attr)
 	/* draw states on id #statesvg */
-	uStates.draw("#statesvg", sampleData, tooltipHtml, attr);
+	// uStates.draw("#statesvg", sampleData, tooltipHtml, attr);
 
-	d3.select(self.frameElement).style("height", "600px");
+	// d3.select(self.frameElement).style("height", "600px");
 }
 
-function getDataBasedonAttribute(attr, data, sample){
+
+
+function getDataBasedonAttribute(attr, data, sample, Fips){
 	var dict={}
 	var low=Math.round(100*Math.random())
 	switch(attr){
 		case "Sex" : dict = getFormattedDataForSex(data,dict);
 					sample = {
-						Ratio:dict[1980] ,
+						State:data["STATE"],
+						Fips:Fips[data["STATE"]],
+						Ratio:dict[2010] ,
                     	color:d3.interpolate("#ffffcc", "rgb(69, 173, 168)")(low/100),
-                		Population:data["Total_Population_1980"]
+                		Population:data["Total_Population_2010"]
 					};
 					return sample;
 		case "Race": sample = {
-						AA_Ratio:data["AA_Ratio_1970"].toFixed(3) ,
-						AI_Ratio:data["AI_Ratio_1970"].toFixed(3) ,
-						APAC_Ratio:data["APAC_Ratio_1970"].toFixed(3) ,
-						W_Ratio:data["W_Ratio_1970"].toFixed(3) ,
+						State:data["STATE"],
+						Fips:Fips[data["STATE"]],
+						AA_Ratio:(data["AA_Ratio_1970"]*100).toFixed(2) ,
+						AI_Ratio:(data["AI_Ratio_1970"]*100).toFixed(2),
+						APAC_Ratio:(data["APAC_Ratio_1970"]*100).toFixed(2) ,
+						W_Ratio:(data["W_Ratio_1970"]*100).toFixed(2) ,
 						color:d3.interpolate("#ffffcc", "rgb(69, 173, 168)")(low/100),
 						Population:data["Total_Population_1970"]
 					};
 					return sample;
-
 
 	}
 
